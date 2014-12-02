0504e90142314170c1d318dd7b41d73c113ca80a
Mikkel Rømer <Mik@Mikkels-MacBook-Pro.local>
2014-12-02 18:32:27 +0100

    Example ussage added to warriner ratings

--- subtitles/subtitleminer/warriner_ratings.py
+++ subtitles/subtitleminer/warriner_ratings.py
@@ -1,10 +1,28 @@
 import csv
 
+"""
+This class is providing the Warriner Rankings of almost 14.000 english words.
+for further information please check, the authors documentation available at:
+http://crr.ugent.be/papers/Warriner_et_al_affective_ratings.pdf
+
+Exampels:
+---------
+>>> w = WarrinerRatings()
+>>> w['attribute_names'][0:3]
+['V.Mean.Sum','V.SD.Sum','V.Rat.Sum','A.Mean.Sum']
+
+>>> w['awesome'][0:3]
+['7.86','1.85','21','6.05']
+
+"""
+
 
 class WarrinerRatings(dict):
-    """Warriner Ratings provided as a dictionary with words as keys"""
+    """Warriner Ratings"""
 
     def __new__(cls):
+        """Constructs a new dictionary containing words and their ratings"""
+
         x = {}
         with open('data/Ratings_Warriner_et_al.csv') as file:
             data = csv.reader(file, delimiter=',')
