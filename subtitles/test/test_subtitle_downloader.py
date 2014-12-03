452ea33e9dec72bc9518cc819f629fe327b0c266
Andreas Piculell <picu-hihi@Picus-lovestick-2.local>
2014-12-03 19:59:46 +0100

    asd

--- subtitles/test/test_subtitle_downloader.py
+++ subtitles/test/test_subtitle_downloader.py
@@ -1,28 +1,28 @@
-from subtitleminer import ImdbMiner
+from subtitleminer import SubtitleDownloader
 
 """test data Pulp Fiction comes with the package"""
 pathname = "data/Pulp_Fiction.en.srt"
 
 
-def test_imdb_fetching_from_url_is_list():
-    info = ImdbMiner().fetch_imdb_releases(range=1)
-    assert type(info) is list
+def test_subtitle_download_from_url_success():
+    srt_name = SubtitleDownloader().download_subtitle_in_srt_from_movie_name('godfather 1972')
+    assert type(srt_name) is str
 
 
-def test_imdb_fetching_from_url_length_of_list():
-    info = ImdbMiner().fetch_imdb_releases(range=1)
-    assert len(info) > 0
-
-
-def test_imdb_fetching_title_is_string():
-    info = ImdbMiner().fetch_imdb_releases(range=1)
-    assert type(info[0][1]) is unicode
-
-
-def test_imdb_fetching_imdbId_is_present():
-    info = ImdbMiner().fetch_imdb_releases(range=1)
-    assert type(info[0][0]) is not None
-
+# def test_imdb_fetching_from_url_length_of_list():
+#     info = ImdbMiner().fetch_imdb_releases(range=1)
+#     assert len(info) > 0
+#
+#
+# def test_imdb_fetching_title_is_string():
+#     info = ImdbMiner().fetch_imdb_releases(range=1)
+#     assert type(info[0][1]) is str
+#
+#
+# def test_imdb_fetching_imdbId_is_present():
+#     info = ImdbMiner().fetch_imdb_releases(range=1)
+#     assert type(info[0][0]) is not None
+#
 
 # def test_that_subtitle_result_is_dictionary_object():
 #     x = StampedSrt(path=pathname)
