7a3a81e666954620e267486215d0c69d96133da5
Mikkel Rømer <Mik@Mikkels-MacBook-Pro.local>
2014-12-05 17:29:45 +0100

    Mikkel forced commit

--- subtitleminer/subtitleminer/__init__.py
+++ subtitleminer/subtitleminer/__init__.py
@@ -3,4 +3,4 @@ from .warriner_ratings import WarrinerRatings
 from .subtitle_features import StampedSrt, SubtitleInIntervals
 from .sentiment_tools import ValenceArouselDominance
 from .subtitle_downloader import SubtitleDownloader
-from .Imdb_miner import ImdbMiner
+from .imdb_miner import ImdbMiner
